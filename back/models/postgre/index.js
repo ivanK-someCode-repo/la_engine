78ae6227e24da60f0ececb50ebb1d4e8e542f3ac
iffan <ivan.kamenkov@gmail.com>
2016-04-02 00:14:32 +0300

    add task description for dao layer design

--- back/models/postgre/index.js
+++ back/models/postgre/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//todo: импорт всех модулей из этой папки
+//через этот файл слои rest api и сервисов бэка будут вызывать модели (т.е. это точка входа в наш слой dao)
+
 var pg = require('pg');
 
 var connectionString = 'postgres://postgres:ParPar52@localhost:5432/engin';//process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
