3ad0ecd7c2598e097da622659d0200c836c9317a
iv <reg_box_2@inbox.ru>
2016-04-01 14:35:31 +0300

    fix liquid

--- liquidbase/run.bat
+++ liquidbase/run.bat
@@ -1,11 +1,11 @@
-call "C:\liquibase\liquibase.bat" ^
+call "C:\liquidbase\liquibase.bat" ^
 --defaultSchemaName=public ^
 --driver=org.postgresql.Driver ^
---classpath="C:\liquibase\postgresql-9.4.1208.jre6.jar" ^
+--classpath="C:\liquidbase\postgresql-9.4.1208.jre6.jar" ^
 --url=jdbc:postgresql://localhost:5432/engin ^
 --username=postgres ^
 --password=ParPar52 ^
---changeLogFile=.\changelog\base.changelog.xml ^
+--changeLogFile=.\changelog\changelog.xml ^
 --logLevel=info ^
 update
 pause
