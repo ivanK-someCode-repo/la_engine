e3874356453451d1ddb03ac798cfa3a7ecdecd34
iffan <ivan.kamenkov@gmail.com>
2016-04-04 23:40:13 +0300

    it serv

--- back/services/item.js
+++ back/services/item.js
@@ -1,3 +1,19 @@
-/**
- * Created by TRUE on 04.04.2016.
- */
+'use strict';
+
+//слой сервисов, индекса, скорее всего, в нем не нужно
+//нужно продумать удобный механизм вызова сервисов для обеспечения взаимодействия рест апи и модели
+//это: валидация данных из рест апи, любая безнес-логика, обращения к сторонним библиотекам
+
+class ItemServices {
+    constructor(modelMethod, parameters) {
+
+    }
+
+    validateParameters(){
+
+    }
+}
+
+module.exports = function(){
+    return new ItemServices();
+};
