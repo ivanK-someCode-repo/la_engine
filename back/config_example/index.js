84d09f6e77035efb2d80911c0789516d0dd1b0d0
iffan <ivan.kamenkov@gmail.com>
2016-04-12 00:11:48 +0300

    fix config

--- back/config_example/index.js
+++ back/config_example/index.js
@@ -7,6 +7,4 @@ nconf.argv()
     .env()
     .file({ file: path.join(__dirname, 'config.json') });
 
-nconf.set('faviconPath', path.join(__dirname, '../www/common/images/favicon.ico'));
-
 module.exports = nconf;
