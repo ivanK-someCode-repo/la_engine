e2ac54c29904ea89b91998b8b46d202835c9c578
iv <reg_box_2@inbox.ru>
2016-04-12 15:32:53 +0300

    add domains to catch async errors

--- app.js
+++ app.js
@@ -1,63 +1,90 @@
-"use strct";
+'use strict';
 
-const http = require('http');
-const express = require('express');
-const url = require('url');
-const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-
-const log = require('./back/libs/log')(module);
 const config = require('./back/config');
-const apiRouter = require('./back/controllers');
-const appRouter = require('./back/routes');
+const log = require('./back/libs/log')(module); //пишут, что хороший логгер - intel
+const domain = require('domain');
+const appDomain = domain.create();
 
-const app = express();
-
-app.set('port', config.get('port'));
-app.set('views', config.get('jsxEngine:layoutsDest'));
-app.set('view engine', config.get('jsxEngine:enginName'));
-app.set('view options', { basedir: config.get('jsxEngine:layoutsDest')});
-//app.locals.basedir = config.get('jsxEngine:layoutsDest');
-
-app.use(favicon(config.get('faviconPath')));
-
-app.use(methodOverride()); // поддержка put и delete
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static('./www'));
-
-http.createServer(app).listen(app.get('port'), function(){
-  log.info('Express server listening on port ' + config.get('port'));
+//домен нужен для перехватывания ошибок в асинхронных вызовах
+appDomain.on('error', function(err){
+  log.error(`appDomain catch error: ${err}`);
 });
 
-app.use('/api', apiRouter); //порядок важен - так api-роуты будут обрабатываться раньше, чем все остальные
-app.use('/', appRouter);
+appDomain.run(function() {
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  log.error('Internal error(%d): %s',res.statusCode,err.message);
+  const http = require('http');
+  const express = require('express');
+  const url = require('url');
+  const favicon = require('serve-favicon');
+  const cookieParser = require('cookie-parser');
+  const bodyParser = require('body-parser');
+  const methodOverride = require('method-override');
+  const apiRouter = require('./back/controllers');
+  const appRouter = require('./back/routes');
+  const app = express();
 
-  if (config.get('env') === 'development') {
-    res.render('error', {
-      message: err.message,
-      error: err
+  app.set('port', config.get('port'));
+  app.set('views', config.get('jsxEngine:layoutsDest'));
+  app.set('view engine', config.get('jsxEngine:enginName'));
+  app.set('view options', { basedir: config.get('jsxEngine:layoutsDest')});
+  //app.locals.basedir = config.get('jsxEngine:layoutsDest');
+
+  app.use(function(err, req, res){
+    huyak();
+  });
+
+  app.use(favicon(config.get('faviconPath')));
+  app.use(methodOverride()); // поддержка put и delete
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static('./www'));
+
+  app.use('/api', apiRouter); //порядок важен - так api-роуты будут обрабатываться раньше, чем все остальные
+  app.use('/', appRouter);
+
+  app.use(function(err, req, res, next){
+    const reqDomain = domain.create();
+
+    reqDomain.add(req);
+    reqDomain.add(res);
+    reqDomain.on('error', function(err){
+      log.error(`reqDomain ${reqDomain} catch error ${err}`);
+      next();
     });
-  }else{
-    //res.send({ error: err.message });
 
-    res.render('error/index', {
-      error: {
-        number: err.status,
-        description: err.message
-      }
-    })
-  }
+    log.error(`appUse catch error ${err}`);
+
+    appDomain.emit('error', err);
+
+    reqDomain.run(next);
+  });
+
+  //возможно, этот мидлвэр нужно будет убрать и добавить вывод ошибки в домен
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    log.error(`Internal error ${res.statusCode}: ${err.message}`);
+
+    if (config.get('env') === 'development') {
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    }else{
+      //res.send({ error: err.message });
+      debugger;
+      res.render('error/index', {
+        error: {
+          number: err.status,
+          description: err.message
+        }
+      })
+    }
+  });
+
+  http.createServer(app).listen(app.get('port'), function(){
+    log.info('Express server listening on port ' + config.get('port'));
+  });
 });
 
 //app.use(logger('dev'));
-
-module.exports = app;
