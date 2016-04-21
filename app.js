96d23bd0eb27e37622d2f23732d4b976cccd495c
ivan <murza-zhut@mail.ru>
2016-04-21 18:16:24 +0300

    work hard on gulp tasks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = require('./back/config');
-const log = require('./back/libs/log')(module); //пишут, что хороший логгер - intel
+const log = require('./back/libs/log')(module); //РїРёС€СѓС‚, С‡С‚Рѕ С…РѕСЂРѕС€РёР№ Р»РѕРіРіРµСЂ - intel
 
 const http = require('http');
 const express = require('express');
@@ -21,24 +21,24 @@ app.set('view options', { basedir: config.get('jsxEngine:layoutsDest')});
 //app.locals.basedir = config.get('jsxEngine:layoutsDest');
 
 app.use(favicon(config.get('faviconPath')));
-app.use(methodOverride()); // поддержка put и delete
+app.use(methodOverride()); // РїРѕРґРґРµСЂР¶РєР° put Рё delete
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('./www'));
 
-app.use('/api', apiRouter); //порядок важен - так api-роуты будут обрабатываться раньше, чем все остальные
+app.use('/api', apiRouter); //РїРѕСЂСЏРґРѕРє РІР°Р¶РµРЅ - С‚Р°Рє api-СЂРѕСѓС‚С‹ Р±СѓРґСѓС‚ РѕР±СЂР°Р±Р°С‚С‹РІР°С‚СЊСЃСЏ СЂР°РЅСЊС€Рµ, С‡РµРј РІСЃРµ РѕСЃС‚Р°Р»СЊРЅС‹Рµ
 app.use('/', appRouter);
 
-//для ловли ошибок возможно пригодится https://github.com/btford/zone.js/
-//для /api нужно прописать отдельный миддлвэр, который не рендерит страницу error
+//РґР»СЏ Р»РѕРІР»Рё РѕС€РёР±РѕРє РІРѕР·РјРѕР¶РЅРѕ РїСЂРёРіРѕРґРёС‚СЃСЏ https://github.com/btford/zone.js/
+//РґР»СЏ /api РЅСѓР¶РЅРѕ РїСЂРѕРїРёСЃР°С‚СЊ РѕС‚РґРµР»СЊРЅС‹Р№ РјРёРґРґР»РІСЌСЂ, РєРѕС‚РѕСЂС‹Р№ РЅРµ СЂРµРЅРґРµСЂРёС‚ СЃС‚СЂР°РЅРёС†Сѓ error
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   log.error(`Internal error ${res.statusCode}: ${err.message}`);
   //next.error();
 
   if (res.headersSent) {
-    //делегирование в стандартные механизмы обработки ошибок в Express, если заголовки уже были отправлены клиенту
+    //РґРµР»РµРіРёСЂРѕРІР°РЅРёРµ РІ СЃС‚Р°РЅРґР°СЂС‚РЅС‹Рµ РјРµС…Р°РЅРёР·РјС‹ РѕР±СЂР°Р±РѕС‚РєРё РѕС€РёР±РѕРє РІ Express, РµСЃР»Рё Р·Р°РіРѕР»РѕРІРєРё СѓР¶Рµ Р±С‹Р»Рё РѕС‚РїСЂР°РІР»РµРЅС‹ РєР»РёРµРЅС‚Сѓ
     return next(err);
   }
 
--- gulpfile.js
+++ gulpfile.js
@@ -15,91 +15,81 @@ const rename = require('gulp-rename');
 const debug = require('gulp-debug');
 const addSrc = require('gulp-add-src');
 
-var concatCss = require('gulp-concat-css');
+const concatCss = require('gulp-concat-css');
 const log = require('gulp-filelog');
+const newer = require('gulp-newer');
+const remember = require('gulp-remember');
 
 //npm install -g "gulpjs/gulp-cli#4.0"
-
 const path = require('path');
+const fs = require('fs');
 
-const dist = 'www';
-const srcBase = 'front';
-
-const paths = [
-	`./${srcBase}/administration/**/*.*`,
-	`./${srcBase}/site/**/*.*`,
-	`./${srcBase}/common/**/*.*`
-];
-
-const pathForTypes = function(postfixes){
-	return paths.map(function(s){
-		return s.slice(0,-1) + postfixes;
-	});
-};
-//['png', 'ico', 'svg']
-//`{${postfix.join(',')}}`
+const DIST = 'www';
+const SRC_BASE = 'front';
+const BOWER_SCR = 'bower_components';
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('clean', function() {
-	return del(dist + '/*');
+	return del(DIST + '/*');
 });
 
 gulp.task('assets', function(){
-	return gulp.src(pathForTypes(`{${['png', 'ico', 'svg'].join(',')}}`), {base: srcBase})
-		.pipe(debug('src'))
+	return gulp.src(`./${SRC_BASE}/**/*.{${['png', 'svg', 'ico'].join(',')}}`, {since: gulp.lastRun('assets')})
+			.pipe(debug({title:'assets'}))
+		.pipe(newer(path.join(DIST, 'customs'))) //check files change date
+			.pipe(debug({title:'assets'}))
 		.pipe(rename(function(filepath) {
 			return filepath.dirname = path.join(filepath.dirname.split(path.sep)[0], 'assets');
 		}))
-		.pipe(debug('rename'))
-		.pipe(gulp.dest(path.join(dist, 'customs')));
+		.pipe(gulp.dest(path.join(DIST, 'customs')));
 });
 
 gulp.task('styles:vendor', function() {
-	return gulp.src(paths)//, {base: sectionsPathsKeys[i]}
-		.pipe()
-		//.pipe(concat('vendor.css'))
-		//.pipe(cssnano())
-
-		.on('data',function(file){
-			console.log(file.path);
-		})
-
-		.pipe(gulp.dest(dist));
-
-
+	return	gulp.src(`./${BOWER_SCR}/**/!(*.*|*-*).css`, {since: gulp.lastRun('styles:vendor')})
+					.pipe(debug({title:'st_vendor'}))
+				.pipe(newer(path.join(DIST, 'vendor')))
+					.pipe(debug({title:'st_vendor'}))
+				.pipe(rename(function(filepath) {
+					return filepath.dirname = '.';// = path.join(filepath.dirname.split(path.sep)[0]);
+				}))
+				.pipe(cssnano())
+				.pipe(gulp.dest(path.join(DIST, 'vendor')));
 });
 
-gulp.task('styles:customs', function() {
-	let pathBase = '';
-	return gulp.src(pathForTypes('less'), {base: srcBase})
-		.pipe(sourcemaps.init())
-			.pipe(less())
-			/*.on('data',function(file){
-				pathBase = file.path.split(path.sep);
-				console.log(file.path.split(path.sep));
-			})*/
-			.pipe(rename(function(filepath) {
-				console.log(filepath.dirname.split(path.sep)[0]);
-				pathBase = filepath.dirname.split(path.sep)[0];
-				return filepath.dirname = filepath.dirname.split(path.sep)[0];
-			}))
-			.on('data',function(file){
-				//pathBase = file.path.split(path.sep);
-				console.log(file.path);
-				console.log(pathBase);
-			})
-			.pipe(concatCss(pathBase + 'main.css'))
-			/*.on('data',function(file){
-				console.log(file.path);
-				console.log(pathBase);
-			})*/
-			.pipe(cssnano())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.join(dist, 'customs')));
-});
+gulp.task('styles:customs', function(callback) {
+	const projectSectionsNames = fs.readdirSync(`./${SRC_BASE}`);
 
-//http://stackoverflow.com/questions/23096103/is-there-a-way-to-dynamically-set-src-and-dest-in-gulpfile-js
+	for (let i = 0; i < projectSectionsNames.length; i++){
+		gulp.src(`./${path.join(SRC_BASE, projectSectionsNames[i])}/**/*.less`, {since: gulp.lastRun('styles:customs')})
+				.pipe(debug({title:'st_customs'}))
+			.pipe(newer(path.join(DIST, 'customs')))
+				.pipe(debug({title:'st_customs'}))
+			.pipe(gulpIf(isDevelopment, sourcemaps.init()))
+				.pipe(less())
+				.pipe(remember('styles:vendor')) //add to stream files that where been ignored by gulp.lastRun
+				.pipe(concatCss(path.join(projectSectionsNames[i], 'main.css')))
+				.pipe(cssnano())
+			.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
+			.pipe(gulp.dest(path.join(DIST, 'customs')));
+	}
+
+	callback();
+});
 
 //gulp.task('styles', gulp.parallel('styles:vendors', 'styles:customs'));
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', gulp.series('clean', gulp.parallel('styles:customs','assets')));
+gulp.task('build', gulp.series(
+						'clean',
+						gulp.parallel('styles:customs', 'styles:vendor', 'assets')));
+
+gulp.task('watch', function(){
+	gulp.watch(`./${SRC_BASE}/**/*.less`, gulp.series('styles:customs'));
+	gulp.watch(`./${BOWER_SCR}/**/!(*.*|*-*).css`, gulp.series('styles:vendor')).on('unlink', function(filepath){
+		//remember.forget('styles:vendor', path.resolve(filepath))
+	});
+	gulp.watch(`./${SRC_BASE}/**/*.{${['png', 'svg', 'ico'].join(',')}}`, gulp.series('assets'));
+	//may add chockidar watch.on('unlink') for unlink deleted files within watch
+});
+
+gulp.task('default', gulp.series('build', 'watch'));
