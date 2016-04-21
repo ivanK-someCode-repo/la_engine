0c3b3320c2976c73739f4b23090a2387513cd731
iffan <ivan.kamenkov@gmail.com>
2016-04-22 00:04:31 +0300

    add sync and watch, add rev

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,14 @@ const del = require('del');
 const rename = require('gulp-rename');
 const debug = require('gulp-debug');
 const addSrc = require('gulp-add-src');
-
+//for tasks correct processing repaired by IDE files when restore files in IDE use gulp-cached
 const concatCss = require('gulp-concat-css');
 const log = require('gulp-filelog');
 const newer = require('gulp-newer');
 const remember = require('gulp-remember');
-
+const rev = require('gulp-rev');
+const browsersync = require('browser-sync').create();
+const through2 = require('through2');
 //npm install -g "gulpjs/gulp-cli#4.0"
 const path = require('path');
 const fs = require('fs');
@@ -35,20 +37,20 @@ gulp.task('clean', function() {
 
 gulp.task('assets', function(){
 	return gulp.src(`./${SRC_BASE}/**/*.{${['png', 'svg', 'ico'].join(',')}}`, {since: gulp.lastRun('assets')})
-			.pipe(debug({title:'assets'}))
+			//.pipe(debug({title:'assets'}))
 		.pipe(newer(path.join(DIST, 'customs'))) //check files change date
-			.pipe(debug({title:'assets'}))
+			//.pipe(debug({title:'assets'}))
 		.pipe(rename(function(filepath) {
 			return filepath.dirname = path.join(filepath.dirname.split(path.sep)[0], 'assets');
-		}))
+			}))
 		.pipe(gulp.dest(path.join(DIST, 'customs')));
 });
 
 gulp.task('styles:vendor', function() {
 	return	gulp.src(`./${BOWER_SCR}/**/!(*.*|*-*).css`, {since: gulp.lastRun('styles:vendor')})
-					.pipe(debug({title:'st_vendor'}))
+					//.pipe(debug({title:'st_vendor'}))
 				.pipe(newer(path.join(DIST, 'vendor')))
-					.pipe(debug({title:'st_vendor'}))
+					//.pipe(debug({title:'st_vendor'}))
 				.pipe(rename(function(filepath) {
 					return filepath.dirname = '.';// = path.join(filepath.dirname.split(path.sep)[0]);
 				}))
@@ -61,16 +63,18 @@ gulp.task('styles:customs', function(callback) {
 
 	for (let i = 0; i < projectSectionsNames.length; i++){
 		gulp.src(`./${path.join(SRC_BASE, projectSectionsNames[i])}/**/*.less`, {since: gulp.lastRun('styles:customs')})
-				.pipe(debug({title:'st_customs'}))
+				//.pipe(debug({title:'st_customs'}))
 			.pipe(newer(path.join(DIST, 'customs')))
-				.pipe(debug({title:'st_customs'}))
+				//.pipe(debug({title:'st_customs'}))
+			.pipe(remember(`styles:customs:${i}`)) //add to stream files that where been ignored by gulp.lastRun
 			.pipe(gulpIf(isDevelopment, sourcemaps.init()))
 				.pipe(less())
-				.pipe(remember('styles:vendor')) //add to stream files that where been ignored by gulp.lastRun
 				.pipe(concatCss(path.join(projectSectionsNames[i], 'main.css')))
-				.pipe(cssnano())
 			.pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
-			.pipe(gulp.dest(path.join(DIST, 'customs')));
+			.pipe(cssnano())
+			.pipe(gulp.dest(path.join(DIST, 'customs')))
+		.pipe(rev.manifest('css'+i+'.json'))
+		.pipe(gulp.dest('./manifest'));
 	}
 
 	callback();
@@ -84,12 +88,32 @@ gulp.task('build', gulp.series(
 						gulp.parallel('styles:customs', 'styles:vendor', 'assets')));
 
 gulp.task('watch', function(){
-	gulp.watch(`./${SRC_BASE}/**/*.less`, gulp.series('styles:customs'));
-	gulp.watch(`./${BOWER_SCR}/**/!(*.*|*-*).css`, gulp.series('styles:vendor')).on('unlink', function(filepath){
-		//remember.forget('styles:vendor', path.resolve(filepath))
+	gulp.watch(`./${SRC_BASE}/**/*.less`, gulp.series('styles:customs')).on('unlink', function(filepath){
+		console.log("wtch stl cust");
+		const projectSectionsNames = fs.readdirSync(`./${SRC_BASE}`);
+
+		console.log(projectSectionsNames);
+		console.log(path.resolve(filepath));
+
+		for (let i=0; i<projectSectionsNames.length; i++){
+			console.log(remember.cacheFor(`styles:customs:${i}`));
+			remember.forget(`styles:customs:${i}`, path.resolve(filepath));
+		}
 	});
+
+	gulp.watch(`./${BOWER_SCR}/**/*.css`, gulp.series('styles:vendor'));
+
 	gulp.watch(`./${SRC_BASE}/**/*.{${['png', 'svg', 'ico'].join(',')}}`, gulp.series('assets'));
-	//may add chockidar watch.on('unlink') for unlink deleted files within watch
 });
 
-gulp.task('default', gulp.series('build', 'watch'));
+gulp.task('serve', function(){
+	browsersync.init({
+		proxy: 'localhost:3000'
+	});
+
+	browsersync.watch(`./${BOWER_SCR}/**/*.*`).on('change', browsersync.reload);
+});
+
+gulp.task('default', gulp.series(
+							'build',
+							gulp.parallel('watch')));//, 'serve'
